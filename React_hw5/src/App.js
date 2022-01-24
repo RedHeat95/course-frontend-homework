87a8d0a2077a3fceca46afea8c7fa78559bffd12
Aleksandr_Shundrik <aleksandrx100@mail.ru>
2022-01-24 15:52:26 +0300

    hw5

--- React_hw5/src/App.js
+++ React_hw5/src/App.js
@@ -1,15 +1,15 @@
 import './App.css';
 
-// import { PostList } from "./components/post/postList/PostList";
-// import { PostFacebookList } from "./components/postFacebook/postFacebookList/PostFacebookList";
-// import { BannerList } from "./components/banner/bannerList/BannerList";
+import { PostList } from "./components/post/postList/PostList";
+import { PostFacebookList } from "./components/postFacebook/postFacebookList/PostFacebookList";
+import { BannerList } from "./components/banner/bannerList/BannerList";
 
 function App() {
   return (
     <div>
-      {/* <PostList /> */}
-      {/* <PostFacebookList /> */}
-      {/* <BannerList /> */}
+      <PostList />
+      <PostFacebookList />
+      <BannerList />
     </div>
   );
 }
