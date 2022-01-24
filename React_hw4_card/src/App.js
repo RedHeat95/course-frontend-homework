08fb6274e18f2909bdafcf1b311371d5eec9edc9
Shundrik <Aleksandrx100@mail.ru>
2022-01-24 20:41:11 +0300

    hw4-new

--- React_hw4_card/src/App.js
+++ React_hw4_card/src/App.js
@@ -1,13 +1,13 @@
 // import logo from './logo.svg';
 import './App.css';
 import { Card } from "./Card";
+import { Card } from "./components/cards/CardTelegram";
 
 function App() {
   return (
     <div>
       <Card />
-
-      
+      <CardTelegram />      
     </div>
   );
 }
